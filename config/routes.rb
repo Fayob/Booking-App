e50e13c78316326e183ab3a69743f998554f60c2
fayob <adedejifavour6@gmail.com>
2023-02-24 14:23:19 +0100

    Add signup and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      post '/signup', to: 'users#signup'
+      post '/login', to: 'users#login'
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
