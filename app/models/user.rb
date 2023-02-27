9f31509451a5e096e4611e3395c1cfe28a6b6dfa
fayob <adedejifavour6@gmail.com>
2023-02-27 14:27:25 +0100

    Validates name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
                        length: { minimum: 3, maximum: 25 }
