f2d09519b90f341ba8dc1bd36463dc2f015675c4
fayob <adedejifavour6@gmail.com>
2023-02-27 20:32:54 +0100

    Add relationship to coach

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :coaches
+  
   validates :name, presence: true
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
