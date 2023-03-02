d5f2c6dc55e2cb1c78271bd856a0997d4e7138fe
fayob <adedejifavour6@gmail.com>
2023-03-02 10:50:36 +0100

    Reserve controller

---
+++ app/controllers/api/v1/reserves_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::ReservesController < ApplicationController
+  def index
+    reserves = @current_user.reserves
+    reserve_coaches = []
+    reserves.each do |reserve|
+      coach = reserve.coach
+      reserve_coaches << coach
+    end
+    render json: reserve_coaches
+  end
+
+  def create
+    reserve = @current_user.reserve.build(coach_id: params[:id])
+    if reserve.save
+      render json: { msg: 'Coach Reversed' }, status: 201
+    else
+      render json: { error: 'Unable to reserve coach' }, status: 422
+    end
+  end
+
+  def destroy
+    reserve = Reserve.find(params[:id])
+    reserve.destroy
+  end
+end
