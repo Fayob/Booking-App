d28b0030fa4974946a310e20939ab9a2dd892c7a
fayob <adedejifavour6@gmail.com>
2023-03-02 10:51:35 +0100

    Catch error in authenticate request method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,7 @@ class ApplicationController < ActionController::API
     header = header.split.last if header
     decoded = jwt_decode(header)
     @current_user ||= User.find(decoded[:user_id]) # rubocop:todo Naming/MemoizedInstanceVariableName
+  rescue TypeError => e
+    'no implicit conversion of Symbol into Integer'
   end
 end
