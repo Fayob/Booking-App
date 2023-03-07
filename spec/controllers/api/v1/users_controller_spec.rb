254a8f4a96fe277f33b7705f7dfe56bcae8c9126
fayob <adedejifavour6@gmail.com>
2023-03-07 19:14:15 +0100

    Fix rubocop error

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -6,20 +6,20 @@ describe Api::V1::UsersController do
       post :signup, params: { name: 'John', username: 'john', password: 'password' }
       expect(response).to have_http_status(:created)
     end
-    
-    it "should throw an error" do
+
+    it 'should throw an error' do
       post :signup, params: { username: 'john', password: 'password' }
       expect(response).to have_http_status(:unprocessable_entity)
     end
   end
-  
+
   describe '#login' do
     # it 'logs in an existing user' do
     #   post :login, params: { username: 'john', password: 'password' }
     #   expect(response).to have_http_status(:ok)
     # end
-  
-    it "should throw an error" do
+
+    it 'should throw an error' do
       post :login, params: { username: 'john' }
       expect(response).to_not have_http_status(:ok)
     end
