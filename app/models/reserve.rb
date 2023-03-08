7a85951a23cb0dbf2b4c770c0a5bc2e662aeacb1
fayob <adedejifavour6@gmail.com>
2023-03-08 11:38:10 +0100

    Add validtaion to date and city column

--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -1,4 +1,7 @@
 class Reserve < ApplicationRecord
   belongs_to :user
   belongs_to :coach
+
+  validates :city, presence: true
+  validates :date, presence: true, comparison: { greater_than_or_equal_to: Date.today }
 end
