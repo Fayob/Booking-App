66b2122cc6e9d270e534e5b9670dfb8d3dbc96b2
fayob <adedejifavour6@gmail.com>
2023-03-09 14:32:19 +0100

    Add test to routes

---
+++ spec/routes/user_routes_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'routes to users controller', type: :routing do
+  it 'should route to signup' do
+    expect(post api_v1_signup_path).to route_to('api/v1/users#signup', format: :json)
+  end
+  it 'should route to login' do
+    expect(post api_v1_login_path).to route_to('api/v1/users#login', format: :json)
+  end
+  it 'should route to not_found' do
+    expect(get '/api/v1/qwee').to route_to('api/v1/users#not_found', format: :json, a: 'qwee')
+  end
+end
